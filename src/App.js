3874e9fa62a6cabdd3620736ea5c45a0832393f0
MarchenkoAndrey61 <xayc755@yandex.ru>
2021-09-16 15:23:37 +0300

    add button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import './App.css';
 import TodoList from "./Todo/TodoList";
 
 function App() {
+    const todos = [
+        {id: 1, completed: false, title: 'Первый таск'},
+        {id: 2, completed: false, title: 'Второй таск'},
+        {id: 3, completed: false, title: 'Третий таск'}
+    ]
+
+
   return (
     <div className="wrapper">
-      <TodoList />
+      <TodoList todos = {todos}/>
     </div>
   );
 }
--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,10 +1,19 @@
 import React from "react";
+import TodoItem from "./TodoItem";
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin : 0,
+        padding : 0
+    }
+}
 
-export default function TodoList() {
+export default function TodoList(props) {
     return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
+        <ul style={styles.ul}>
+            {props.todos.map((todo, index) => {
+                return <TodoItem todo = {todo} key = {todo.id} index = {index}/>
+            })}
         </ul>
     )
 }
